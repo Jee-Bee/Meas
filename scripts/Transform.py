2b8892bd1521c02fc072d16a8a239c02498823ce
Jee-Bee <gewoon.jeebee@gmail.com>
2016-02-11 15:15:44 +0100

    add exeptions to transform

--- scripts/Transform.py
+++ scripts/Transform.py
@@ -25,16 +25,48 @@ def NFFT (data):
 
 def FFT (data,fs,*args, **kwargs):
     # sig, fs,window,smoothing, 
-    # spectrum = complex(=real+imag)/amp+phase, Shift = yes # removed: side = singele/ double sided
+    # spectrum = complex(=real+imag)/amp+phase, Shift = True # removed: side = singele/ double sided
+    nfft = NFFT(data)
     if args == []:
-        pass
+        DATA = ft.fft(data, nfft)
+    elif len(args) <= 2:
+        if len(args) == 2:
+            if isinstance(args[1],str)== True:
+#                Window_Type = argv[1]
+                pass
+            else:
+                print ("Window type not the right input type")
+            # argv[2]
+            if isinstance(args[2],int)== True:
+#                smoothing = int(args[2])
+                pass
+            elif isinstance(args[2], float) == True:
+                if args[2]%1 == 0:
+#                    smoothing = int(args[2])
+                    pass
+            else:
+                print ("Smooting not the right input type")
+        else:
+            if isinstance(args[1],str)== True:
+#                Window_Type = argv[1]
+                pass
+            elif isinstance(args[1],int)== True:
+#                smoothing = int(args[1])
+                pass
+            elif isinstance(args[1], float) == True:
+                if args[1]%1 == 0:
+#                    smoothing = int(args[1])
+                    pass
+            else:
+                print ("not the right input type")
     else:
+        print ("not the right number of parameters")
         print(args)
     if kwargs == []:
         pass
     else:
         print (kwargs)
-    nfft = NFFT(data)
+    
     DATA = ft.fft(data, nfft)
     print(fs)
     return (DATA)
