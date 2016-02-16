2a98380dde43247f3aeb7ec672c1cd311c6494ab
Jee-Bee <gewoon.jeebee@gmail.com>
2016-02-16 16:08:43 +0100

    fill part of UnitsDB

--- scripts/UnitsDB.py
+++ scripts/UnitsDB.py
@@ -5,3 +5,13 @@ Created on Mon Feb 15 16:24:35 2016
 @author: Jee-Bee for jBae (c) 2016
 """
 
+
+#Units name : Description
+Units = {'Frequency': 'Frequency [Hz]',
+         'Gravity':'Amplitude [g]',
+         'Power Watt': 'Power [W]',
+         'Power Volt': 'power [V^2]',
+         'Time': 'Time [s]',
+         'Volt': 'Amplitude [v]',
+         'Volt RMS': 'Amplitude [V_rms]', 
+         }
