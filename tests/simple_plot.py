528c3484a85b116c34fe01a2ff5f6126cdff59af
Jee-Bee <gewoon.jeebee@gmail.com>
2016-02-19 15:27:24 +0100

    small changes start overlab characteristics

--- tests/simple_plot.py
+++ tests/simple_plot.py
@@ -12,3 +12,36 @@ plt.title('About as simple as it gets, folks')
 plt.grid(True)
 plt.savefig("test.png")
 plt.show()
+
+#
+# ----------- From Here Overlab characteristics function ---------------------
+#
+import sys
+from ..scripts import Window
+import numpy as np
+
+
+def Overlap_Characterestics(Window_type, accurency):
+#        Calculate AF = Min/Max
+#        Calculate PF
+#        Calculate OC 
+#        See 395068 Window Document: 
+#            Spectrum and spectral density estimation by the Discrete Fourier
+#            transform (DFT), including a comprehensive list of window
+#            functions and some new at-top windows.
+    N = 1024  # Window Length
+    Overlab = 50  # [%] 
+    Overlab_var = Overlab/100 + 1
+    hann = Window.hanwind(N)
+    y= np.zeros(N* Overlab_var)
+    y = y + hann
+    x = np.arange(N*Overlab_var)
+    return (x,y)
+#    pass
+
+ 
+def ROV (self, Window_type):
+#        Input are values from Input characteresitcs
+    pass
+
+
