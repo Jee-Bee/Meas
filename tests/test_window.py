d54b050de486f44d64670c7700940a948e505761
Jee-Bee <gewoon.jeebee@gmail.com>
2016-02-19 14:45:04 +0100

    major cleanup

--- tests/test_window.py
+++ tests/test_window.py
@@ -16,119 +16,7 @@ import numpy as np
 from scipy.io import wavfile
 from scipy.fftpack import fft, fftshift
 import matplotlib.pylab as plt
-
-# B slpine Windows:
-# Rectangular: k = 1 (st order)
-# Triangular: k = 2 (nd Order)
-# Parzen: k = 4 (th order)
-
-def rectwind(N):
-    w = np.ones(N)
-    x = np.arange(0,N-1,1)
-    return(w,x)
-
-def triwind(N):
-    w=np.zeros(N)
-    x=np.zeros(N)
-    for idx in range(N):
-        w[idx] = 1-abs((idx-((N-1)/2))/(N/2))
-        x[idx] = idx
-    return (w,x)
-
-def partzwind(N):
-    w=np.zeros(int(N/2))
-    x=np.zeros(int(N/2))
-    for idx in range(int(N/2)):
-        x[idx] = idx
-        if (idx >= 0) and (idx <= (N/4)):
-            w[idx] = 1-(6*(idx/(N/2))**2) * (1- abs(idx)/(N/2))
-        elif(idx >= N/4) and (idx <= (N/2)):
-            w[idx] = 2*(1- abs(idx)/(N/2))**3
-    x = np.append(x,x +int(N/2));
-    w = np.append(w[::-1],w)
-    return (x,w)
-##    if (idx < 3) == True:
-
-#
-# -----------------------------------------------------------------------------
-#
-
-def genhamwind(N,alpha,beta):
-    w=np.zeros(N)
-    x=np.zeros(N)
-    for idx in range(N):
-        w[idx] = alpha - beta * np.cos((2*np.pi*idx)/(N-1))
-        x[idx] = idx
-    return (x,w)
-
-def hanwind(N):
-    alpha = beta = 0.5
-    [x,w] = genhamwind(N,alpha,beta)
-    return (x,w)
-
-def hamwind(N):
-    alpha =  0.53836
-    beta = 1 - alpha
-    [x,w] = genhamwind(N,alpha,beta)
-    return (x,w)
-
-#
-# -----------------------------------------------------------------------------
-#
-
-def coswind (N):
-    w = np.zeros(N)
-    x = np.zeros(N)
-    alpha = 1  # rectangular window alpha = 0; cos window alpha = 1; Hann window alpha =2.
-    for idx in range(N):
-        w[idx] = np.cos((np.pi * idx/(N-1)) - np.pi/2)**alpha
-        x[idx] = idx
-    return (x,w)
-
-#
-# -----------------------------------------------------------------------------
-#
-
-def gengausswind(N,sigma,p): 
-    w = np.zeros(N)
-    x= np.zeros(N)
-    for idx in range(N):
-        num = idx-(N-1)/2
-        denum = sigma*(N-1)/2
-        w[idx] = np.e**((-1/2)*( num/denum )**p )
-        x[idx] = idx
-    return (x,w)
-
-def gausswind(N): 
-    sigma = 0.5
-    p = 2
-    [x,w] = gengausswind(N,sigma,p)
-    return (x,w)
-
-#
-# -----------------------------------------------------------------------------
-#
-
-def Tukeywind (N):
-    w = np.zeros(N)
-    x = np.zeros(N)
-    alpha = 0.5
-    for idx in range(N):
-        if idx <= int(alpha*(N-1)/2):
-            w[idx] = 1/2*(1 + np.cos(np.pi*(2*idx/(alpha * (N-1)) -1) ) )
-            x[idx] = idx
-        elif idx >= int(alpha*(N-1)/2) and idx <= int((N-1)*(1-alpha/2)):
-            w[idx] = 1
-            x[idx] = idx
-        elif idx >= int((N-1)*(1-alpha/2)) and idx <= (N-1):
-            w[idx] = 1/2*(1 + np.cos(np.pi*(2*idx/(alpha * (N-1)) - 2/alpha + 1) ) )
-            x[idx] = idx
-    return (x,w)
-
-
-#
-# -----------------------------------------------------------------------------
-#
+from ..scripts import Window
 
 # Vars
 N = int(1024)
@@ -175,9 +63,9 @@ elif method == 'file':
 
 #N = 256
 
-[wt,x] = triwind(N)
-[x,whan] = hanwind(N)
-[x,wcos] = coswind(N)
+[wt,x] = Window.triwind(N)
+[x,whan] = Window.hanwind(N)
+[x,wcos] = Window.coswind(N)
 
 S_1 = np.sum(whan)
 S_2 = np.sum(whan**2)
@@ -260,6 +148,14 @@ for idx in range(1,itt+1):
     idxup = (idx*N)-1
     MWIND_nf[idx-1] = fftshift(fft(data_nf[(idx-1)*N:(idx*N):]*whan))
 MWIND_nf = np.rot90(MWIND_nf,3)
+
+## Implement Later!! First read Paper
+whan_ls = np.zeros((len(data_nf),itt))  # Long signal
+for idx in range(itt):
+#    whan_ls[idx*N:(idx+1)*N:][idx] = whan
+    whan_ls[(idx*N):((idx+1)*N),idx] = whan
+whan_ls =np.sum(whan_ls, axis=1)
+#MWIND_ls = fftshift(fft(data_nf[(idx-1)*N:(idx*N):]*whan))
 
 #itt1 = data_nf[0:N:]
 #square3 = np.array([[1,2,3],[4,5,6],[7,8,9]])
