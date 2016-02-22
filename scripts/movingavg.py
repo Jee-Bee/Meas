458b8f22d756a6eaf87194afa995ae63cdd27dd1
Jee-Bee <gewoon.jeebee@gmail.com>
2016-02-22 22:02:47 +0100

    Add Cummulative moving average

--- scripts/movingavg.py
+++ scripts/movingavg.py
@@ -14,7 +14,8 @@ import numpy as np
 # SMA = - * ------------------------------- = -   SUM p_(m-i)
 #       n                 n                   n   i=0
 
-def sma(x, npa):
+
+def sma(x, npa):  # npa: Number P_Average = length Center point
     sma = np.zeros(len(x))
     if int(npa) % 2 == 0:
         npa = int(npa - 1)
@@ -28,27 +29,22 @@ def sma(x, npa):
                 npasum = npasum + 0
             else:
                 npasum = npasum + x[idx1 + npaidx]
-        sma[idx1]= npasum / npa
+        sma[idx1] = npasum / npa
     return(sma)
 
-# Cumelative Moving Average
 
-#def sma(x,npa):
-#    sma = np.zeros(len(x))    
-#    if int(npa) % 2 == 0:
-#        npa = int(npa - 1)
-#    for idx1 in range(len(x)):  # calculate mean/ average
-#        npasum = 0
-#        for idx2 in range(int(npa)):  # check for zeros
-#            npaidx = idx2 - np.floor(npa/2)
-#            if (idx1 + npaidx) < 0:
-#                npasum = npasum + 0
-#            elif (idx1 + npaidx) >= len(x):
-#                npasum = npasum + 0
-#            else:
-#                npasum = npasum + x[idx1+npaidx]
-#        sma[idx1]= npasum/npa
-#    return (sma)
+# Cumelative Moving Average
+def cma(x1, xn):
+    cma = np.zeros(len(x1))
+    n = len(x1)
+    cma = np.sum(x1)/n  # calculate mean/ average
+    if len(xn) == 0:
+        return(cma)
+    else:
+        cman = np.zeros(len(xn))
+        for idx in range(len(xn)):
+            cman = (cma + n * xn[idx]) / (n + 1)
+        return (cman)
 
 # Weighted Moving Average
 
