cc0c21d5bd5ff9490897962612870f03db47d2bc
Jee-Bee <gewoon.jeebee@gmail.com>
2016-02-25 22:08:47 +0100

    add multple units to list

--- scripts/UnitsDB.py
+++ scripts/UnitsDB.py
@@ -6,11 +6,33 @@ Created on Mon Feb 15 16:24:35 2016
 """
 
 #Units name : Description
-Units = {'Frequency': 'Frequency [Hz]',
-         'Gravity':'Amplitude [g]',
-         'Power Watt': 'Power [W]',
-         'Power Volt': 'power [V^2]',
-         'Time': 'Time [s]',
-         'Volt': 'Amplitude [v]',
-         'Volt RMS': 'Amplitude [V_rms]', 
+Units = {'Acceleration': 'Acceleration $[m/s^2]$',
+         'Decibel 1mW': 'Amplitude $[dB_{m}]$',  # ref 1mW @ 600 $\ohm$
+         'Decibel SPL': 'Amplitude $[dB_{SPL}]$',
+         'Decibel 1Volt': 'Amplitude $[dB_{v}]$',  # ref 1V
+         'Decibel 0.707Volt RMS': 'Amplitude $[dB_{V}]$',  # ref 0.707V_{RMS}
+         'Displacement': 'Displacement $[m]$',
+         'Frequency Bins': 'Frequency Bins $[-]$',
+         'Frequency Cycle': 'Frequency $[CPM]$',
+         'Frequency': 'Frequency $[Hz]$',
+         'Frequency RPM': 'Frequency $[RPM]$',
+         'Gravity': 'Amplitude $[g]$',
+         'Gravity RMS': 'Amplitude $[g_{RMS}]$',
+         'Phase deg': 'Phase $[{}^O]$',
+         'Phase deg u': 'Phase Unwraped $[{}^O]$',
+         'Phase rad': 'Phase $[\pi RAD]$',
+         'Phase rad u': 'Phase Unwraped $[\pi RAD]$',
+         'Power Watt': 'Power $[W]$',
+         'Power Volt': 'Power $[V^2]$',
+         'PSD Volt': 'PSD $[\tfrac{V^2}{Hz}]',
+         'Pressure': 'Pressure $[Pa]$',
+         'Quefrency': 'Quefrency $[s-samples]$',  # pitch
+         'SD Volt': 'SD $[\tfrac{V}{\sqrt{Hz}}]',
+         'Time sec': 'Time $[s]$',
+         'Time samp': 'Time $[-]$',
+         'Velocity': 'Velocity $[m/s]$',
+         'Volt': 'Amplitude $[V]$',
+         'Volt RMS': 'Amplitude $[V_{RMS}]$',
          }
+
+# http://stackoverflow.com/questions/12815830/latex-symbols-with-matplotlib
