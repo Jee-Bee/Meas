56d3573df1ac4a22ac7aa1b857a24dce7188aa2f
Jee-Bee <gewoon.jeebee@gmail.com>
2016-03-13 21:54:44 +0100

    add Meas warning initial start

---
+++ scripts/MeasWarning.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 11 10:42:34 2016
+
+@author: Jee-Bee for Jbae (c) 2016
+"""
+import sys
+import warning
+
+
+class MeasWarning(Warning):
+    """Base class for Meas Warnings handling"""
+    pass
+
