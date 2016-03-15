565c5f80c9022c4bbe9b86934703ba9064b5c7ea
Jee-Bee <gewoon.jeebee@gmail.com>
2016-03-15 19:58:54 +0100

    initial Gui workarounds

---
+++ MeasMaster.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  6 13:04:25 2016
+
+@author: Jee-Bee for jBae (c) 2016
+"""
+from PyQt5 import QtCore, QtGui, QtWidgets
+from ui.jBae_Meas_0_15 import Ui_MeasMain
+import sys
+
+
+# http://stackoverflow.com/questions/14755305/open-a-gui-file-from-another-file-pyqt#14756580
+# main window is child. this file mother file!!
+class Meas(object):
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        
+#self.setWindowIcon(QtGui.QIcon('pythonlogo.png'))
+
+    def showmeas(self):
+        self.child_wind = Ui_MeasMain()
+        self.ui_MeasMain.setupUi.actionExit = QtWidgets.QAction(MeasMain)
+        self.ui_MeasMain.retranslateUI.setWindowTitle("Meas Sound Measurement Tool")
+        self.child_wind.show()
+
+    def exit(self):
+        choice = QtGui.QMessageBox.question(self, 'Exit Meas',
+                    "Are You sure to Leave Meas?", QtGui.QMessageBox.Yes | QtGui.QMessageBox.No)
+        if choice == QtGui.QMessageBox.Yes:
+            sys.exit()
+        else:
+            pass
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    MeasMain = QtWidgets.QMainWindow()
+    ui = Ui_MeasMain()
+    ui.setupUi(MeasMain)
+    MeasMain.show()
+    sys.exit(app.exec_())
