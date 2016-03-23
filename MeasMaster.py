f8018ffed5f7a80030acaa2226339eec6a7d6805
Jee-Bee <gewoon.jeebee@gmail.com>
2016-03-23 09:39:27 +0100

    Update MeasMaster.py
    run now from Meas class not from Ui_MeasMain

--- MeasMaster.py
+++ MeasMaster.py
@@ -6,22 +6,27 @@ Created on Sun Mar  6 13:04:25 2016
 """
 from PyQt5 import QtCore, QtGui, QtWidgets
 from ui.jBae_Meas_0_15 import Ui_MeasMain
+from resources.icons import *
 import sys
 
 
+# http://blog.abstractfactory.io/dynamic-signals-in-pyqt/
+# http://stackoverflow.com/questions/26867723/how-to-call-a-python-script-on-button-click-using-pyqt
 # http://stackoverflow.com/questions/14755305/open-a-gui-file-from-another-file-pyqt#14756580
 # main window is child. this file mother file!!
-class Meas(object):
+class Meas(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
-        QtGui.QWidget.__init__(self, parent)
-        
-#self.setWindowIcon(QtGui.QIcon('pythonlogo.png'))
+        #QtGui.QWidget.__init__(self, parent)
+        super(Meas, self).__init__(parent)
+        self.ui = Ui_MeasMain()
+        self.ui.setupUi(self)
+        # self.ui.setWindowIcon(QtWidgets.QIcon('MeasLogoSm.png'))
 
-    def showmeas(self):
-        self.child_wind = Ui_MeasMain()
-        self.ui_MeasMain.setupUi.actionExit = QtWidgets.QAction(MeasMain)
-        self.ui_MeasMain.retranslateUI.setWindowTitle("Meas Sound Measurement Tool")
-        self.child_wind.show()
+#    def showmeas(self):
+#        self.child_wind = Ui_MeasMain()
+#        self.ui_MeasMain.setupUi.actionExit = QtWidgets.QAction(MeasMain)
+#        self.ui_MeasMain.retranslateUI.setWindowTitle("Meas Sound Measurement Tool")
+#        self.child_wind.show()
 
     def exit(self):
         choice = QtGui.QMessageBox.question(self, 'Exit Meas',
@@ -33,8 +38,6 @@ class Meas(object):
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    MeasMain = QtWidgets.QMainWindow()
-    ui = Ui_MeasMain()
-    ui.setupUi(MeasMain)
-    MeasMain.show()
+    measapp = Meas()
+    measapp.show()
     sys.exit(app.exec_())
