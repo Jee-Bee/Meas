d6a800c100fb796e3a29b203c83f51c7d5e17793
Jee-Bee <gewoon.jeebee@gmail.com>
2016-03-25 21:43:33 +0100

    change exit function for better method see link

--- MeasMaster.py
+++ MeasMaster.py
@@ -37,6 +37,7 @@ class Meas(QtWidgets.QMainWindow):
 #        self.ui_MeasMain.retranslateUI.setWindowTitle("Meas Sound Measurement Tool")
 #        self.child_wind.show()
 
+<<<<<<< 21256eea7d52e994e65e95429fa712972887e8f1
     def Exit(self):
         choice = QtGui.QMessageBox.question(self, 'Exit Meas',
                     "Are You sure to Leave Meas?", QtGui.QMessageBox.Yes | QtGui.QMessageBox.No)
@@ -44,6 +45,15 @@ class Meas(QtWidgets.QMainWindow):
             sys.exit()
         else:
             pass
+=======
+# http://stackoverflow.com/questions/36180552/linking-pyqt-files-dont-relate/
+    def Exit(self):
+        choice = QtWidgets.QMessageBox.question(self, 'Exit Meas',
+                    "Are You sure to Leave Meas?",
+                    QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No)
+        if choice == QtWidgets.QMessageBox.Yes:
+            self.close()
+>>>>>>> change exit function for better method see link
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
