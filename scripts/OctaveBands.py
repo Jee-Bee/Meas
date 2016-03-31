4f11aa8fc7003b9c15605b62dbc5bae49cf9b96e
Jee-Bee <gewoon.jeebee@gmail.com>
2016-03-31 19:49:28 +0200

    forgot final save

--- scripts/OctaveBands.py
+++ scripts/OctaveBands.py
@@ -159,36 +159,3 @@ def OctaveN(Measurement, F, *args, **kwargs):
                 bands[a] = bands[a] + (F[idx[0] + b] - F[idx[0] + b - 1]) * abs(Measurement[idx[0] + b] + Measurement[idx[0] + b - 1]) / 2
             bands[a] = bands[a] / (F[idx[len(idx) - 1]] - F[idx[0]])
     return (Freq_pref, bands)
-
-
-
-#np.abs(np.array(np.random.rand(T * fs / 2)))
-T = 5
-fs = 44100
-F = np.arange(1/T, fs/2, 1 / T )
-oct_array = OctaveN(np.abs(np.array(np.random.rand(T * fs / 2))), F)
-
-#Small tests - delete later on:
-#    a = np.array([[1 ,2], [3, 4], [5,6]])
-#    a[1] = array([3,4])
-#    a[1][1] = 4
-#
-#    for idx in range(len(a)):
-#        print(a[idx][0])
-#
-#    b = np.where((a>=3) & (a<=5)) = (array([1, 1, 2], dtype=int64), array([0, 1, 0], dtype=int64))
-
-
-#Example:
-#T =3
-#f = 1000 Hz #= basic frequency
-#np.log10(1000/1) = 3
-#np.log10(1000/(1/T)) = 3.477...
-#
-#np.log2(1000/(1/T)) = 11.55...
-#but since formulation octave is:
-#1000*2**(n/2) or 1000/2**(n/2)
-#i need to compensate my factor 2 so...
-#1000 / 2**((2*12)/2) = 0.244... is around 0.33 Hz what was the signal
-
-# 1/n test = 1/24
