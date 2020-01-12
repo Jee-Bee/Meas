6c4317e319a3ec0f773598dbb8d0aab8c4448230
Jee-Bee <gewoon.jeebee@gmail.com>
2020-01-12 12:57:53 +0100

    move addzeros, simplify srepeat and add repAvg

--- src/repeat.py
+++ src/repeat.py
@@ -11,61 +11,62 @@ Created on Sat Jul 16 14:47:07 2016
 
 import numpy as np
 
-def srepeat(sig, reps, l0=None, fs=None, addzeros=False):
+
+def addzeros(signal, l0, fs=None):
     """
+    Input:
+        sig
+        l0 = length in samples or together with fs (sample frequency) time T
+        fs(optional) = sample Frequency
+    Output:
+        repsig = repeated signal"""
+    if fs is not None:
+        zero_array = np.zeros(np.int(np.round(l0 * fs)))
+    else:
+        zero_array = np.zeros(np.int(l0))
+    signalwzeros = np.append(signal, zero_array)
+    return(signalwzeros)
+
+
+def srepeat(sig, reps, l0=None, fs=None):
+    """ Single repeat:
+    Repeat signal n times. l0 is non None than zeros are added.
     Input:
         sig
         reps
         l0(Optional) = length in samples or if it an array T(length time) together
         with fs (sample frequency)
         fs(optional) = sample Frequency
-        addzeros = True
     Output:
         repsig = repeated signal
-        ursl = unrepeatedsignal length
-
-    TODO:
-    - Make ursl optional
+        sing_length = length of signal
     """
-    if addzeros is True:
-        if (l0 is None) and (fs is None):
-            raise ValueError("if addzeros is True l0 AND fs can't be None")
-        elif (fs is None) and l0 < 1024:
-            raise ValueError("if addzeros is True and fsis None. l0 should be greather than 1024 samples")
-        elif (fs is None) and l0 > 1024:
-            sig = np.append(sig, np.zeros(l0))
-            repsig = np.tile(sig, reps)
-            return(repsig, len(sig))
-        else:
-            sig = np.append(sig, np.zeros(l0*fs))
-            repsig = np.tile(sig, reps)
-            return(repsig, len(sig))
-    else:
-        repsig = np.tile(sig, reps)
-        return(repsig, len(sig))
+    if l0 is not None:
+        sigwzeros = addzeros(sig, l0, fs)
+    repsig = np.tile(sig, reps)
+    return(repsig, len(sig))
 
 
-def repAvg(repsig, reps, l0=None):
+def repAvg(repeated_sig, repeats, l0=None):
     """
     Input:
-        repsig = measured repeated signal
-        reps = number of repeats
+        repeated_sig = measured repeated signal
+        repeats = number of repeats
         l0(optional) = time between repeats... - Not implemented yet
     Output:
-        avgsig = averaged signal
-
+        averaged_sig = averaged signal
     TODO:
     - implement l0
     """
-    if len(repsig) % reps == 0:
-        N = int(len(repsig) / reps)  # signal Length singel signal
+    arr_dim = repeated_sig.ndim
+    if arr_dim == 1:
+        averaged_sig = repeated_sig.reshape(repeats, -1).sum(axis=0) / repeats
+        return(averaged_sig)
     else:
-        raise ValueError("length of signal can't be divided by number of repeats")
-    avgsig = np.zeros(N)
-    for idx in range(reps):
-        avgsig += repsig[idx * N:(idx+1) * N]
-    avgsig = avgsig / reps
-    return(avgsig)
+        print("be carefull dimension and layout arrray not tested!!")
+        channels = repeated_sig.shape[0]  # of 1
+        averaged_sig = repeated_sig.reshape(channels, repeats, -1).sum(axis=1) # check output of
+        return(averaged_sig)
 
 
 def mrepeat(sig, reps, channels, l0=None, fs=None, addzeros=False, method=1):
@@ -88,3 +89,20 @@ def mrepeat(sig, reps, channels, l0=None, fs=None, addzeros=False, method=1):
     else:
         raise ValueError("method should be a 1 or a 2")
     pass
+
+# repeats = 2
+# c = np.arange(6)
+# d = c.reshape(repeats,-1)
+# # c = array([0, 1, 2, 3, 4, 5])
+# # d = array([[0, 1, 2],
+# #            [3, 4, 5]])
+# a=np.arange(12).reshape(-1,6)  # virtual 2 channels recorded
+# a.shape  # = (2, 6)
+# channels = a.shape[0]
+# # array([[ 0,  1,  2,  3,  4,  5],
+# #        [ 6,  7,  8,  9, 10, 11]])
+# b = a.reshape(channels,repeats,-1)
+# # b = array([[[ 0,  1,  2],
+# #             [ 3,  4,  5]],
+# #            [[ 6,  7,  8],
+# #             [ 9, 10, 11]]])
